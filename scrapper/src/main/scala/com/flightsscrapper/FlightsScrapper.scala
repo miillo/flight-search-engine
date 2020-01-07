31da7f2b3113cabb4b21498528b6dbb880b54424
Miłosz Pluta <milosz.pluta2@gmail.com>
2020-01-07 22:25:38 +0100

    Actor system init

--- scrapper/src/main/scala/com/flightsscrapper/FlightsScrapper.scala
+++ scrapper/src/main/scala/com/flightsscrapper/FlightsScrapper.scala
@@ -1,11 +1,18 @@
 package com.flightsscrapper
 
+import akka.actor.ActorSystem
 import com.flightsscrapper.configuration.ApplicationProperties
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 object FlightsScrapper {
 
   def main(args: Array[String]): Unit = {
     val appProperties = new ApplicationProperties("scrapper/src/resources/application.conf")
-    print(appProperties.sourceDataPath)
+
+    implicit val system: ActorSystem = ActorSystem("FlightsScrapper ")
+
+    Await.result(system.whenTerminated, Duration.Inf)
   }
 }
